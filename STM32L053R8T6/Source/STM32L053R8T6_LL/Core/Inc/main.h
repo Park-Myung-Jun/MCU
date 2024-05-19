058d70a13a6218a6b9f1eb84ced0e1bfd1171d91
PARK-MYUNG-JUN <juneblack@naver.com>
2024-05-19 23:28:35 +0900

    <tim>
    1. tim HAL, LL compare

---
+++ STM32L053R8T6/Source/STM32L053R8T6_LL/Core/Inc/main.h
@@ -0,0 +1,100 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.h
+  * @brief          : Header for main.c file.
+  *                   This file contains the common defines of the application.
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2024 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef __MAIN_H
+#define __MAIN_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* Includes ------------------------------------------------------------------*/
+#include "stm32l0xx_hal.h"
+
+#include "stm32l0xx_ll_tim.h"
+#include "stm32l0xx_ll_bus.h"
+#include "stm32l0xx_ll_cortex.h"
+#include "stm32l0xx_ll_rcc.h"
+#include "stm32l0xx_ll_system.h"
+#include "stm32l0xx_ll_utils.h"
+#include "stm32l0xx_ll_pwr.h"
+#include "stm32l0xx_ll_gpio.h"
+#include "stm32l0xx_ll_dma.h"
+
+#include "stm32l0xx_ll_exti.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include <string.h>
+#include <stdint.h>
+#include <stdbool.h>
+#include <time.h>
+/* USER CODE END Includes */
+
+/* Exported types ------------------------------------------------------------*/
+/* USER CODE BEGIN ET */
+
+/* USER CODE END ET */
+
+/* Exported constants --------------------------------------------------------*/
+/* USER CODE BEGIN EC */
+
+/* USER CODE END EC */
+
+/* Exported macro ------------------------------------------------------------*/
+/* USER CODE BEGIN EM */
+
+/* USER CODE END EM */
+
+/* Exported functions prototypes ---------------------------------------------*/
+void Error_Handler(void);
+
+/* USER CODE BEGIN EFP */
+
+/* USER CODE END EFP */
+
+/* Private defines -----------------------------------------------------------*/
+#define BUTTON_BLUE_PC13_Pin GPIO_PIN_13
+#define BUTTON_BLUE_PC13_GPIO_Port GPIOC
+#define BUTTON_BLUE_PC13_EXTI_IRQn EXTI4_15_IRQn
+#define USART2_TX_PA2_Pin GPIO_PIN_2
+#define USART2_TX_PA2_GPIO_Port GPIOA
+#define USART2_RX_PA3_Pin GPIO_PIN_3
+#define USART2_RX_PA3_GPIO_Port GPIOA
+#define LED_GREEN_PA5_Pin GPIO_PIN_5
+#define LED_GREEN_PA5_GPIO_Port GPIOA
+#define USB_DM_PA11_Pin GPIO_PIN_11
+#define USB_DM_PA11_GPIO_Port GPIOA
+#define USB_DP_PA12_Pin GPIO_PIN_12
+#define USB_DP_PA12_GPIO_Port GPIOA
+
+/* USER CODE BEGIN Private defines */
+
+/* USER CODE END Private defines */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __MAIN_H */
